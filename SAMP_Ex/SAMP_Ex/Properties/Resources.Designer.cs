3694d1112ff0abcce5e74acc6e9cd597f33be72f
Papawy <contactpapawy@gmail.com>
2016-08-02 23:37:58 +0200

    project resources

--- SAMP_Ex/SAMP_Ex/Properties/Resources.Designer.cs
+++ SAMP_Ex/SAMP_Ex/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace SAMP_Ex.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap folder {
+            get {
+                object obj = ResourceManager.GetObject("folder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap garbage {
             get {
                 object obj = ResourceManager.GetObject("garbage", resourceCulture);
