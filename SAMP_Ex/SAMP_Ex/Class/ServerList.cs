4074e8b6508e8f3dacc508f4de7335c74eb1664b
PAPAWY-PCPORT\matho <contactpapawy@gmail.com>
2016-08-18 14:42:41 +0200

    Added serverlist control to show servers in a list with a bit of informations (the same as the original launcher)

---
+++ SAMP_Ex/SAMP_Ex/Class/ServerList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SAMP_Ex
+{
+    class ServerList : DataGridView
+    {
+        public Image LockedImage { get; set; }
+        public Image UnlockedImage { get; set; }
+
+        public ServerList() : base()
+        {
+            this.RowHeadersVisible = false;
+            this.AllowUserToAddRows = false;
+            this.AllowUserToDeleteRows = false;
+            this.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            System.Windows.Forms.DataGridViewImageColumn lockColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            lockColumn.Image = LockedImage;
+
+            this.Columns.Add(lockColumn);
+            lockColumn.HeaderText = "Locked";
+            lockColumn.Name = "locked";
+
+            this.Columns.Add("hostname", "Hostname");
+            this.Columns.Add("players", "Players");
+            this.Columns.Add("ping", "Ping");
+            this.Columns.Add("mode", "Mode");
+            this.Columns.Add("language", "Language");
+
+            this.Columns["locked"].Width = 20;
+            this.Columns["locked"].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+
+        }
+
+        public void AddServer(Server server)
+        {
+            if (server.HasPassword)
+            {
+                this.Rows.Add(LockedImage,
+                server.Hostname,
+                server.Players + "/" + server.MaxPlayers,
+                server.Ping.ToString(),
+                server.Gamemode,
+                server.Language);
+            }
+            else
+            {
+                this.Rows.Add(UnlockedImage,
+                server.Hostname,
+                server.Players + "/" + server.MaxPlayers,
+                server.Ping.ToString(),
+                server.Gamemode,
+                server.Language);
+            }
+        }
+    }
+}
