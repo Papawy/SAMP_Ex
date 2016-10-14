71faf06301c9f3991b0c57cb547cdbe658b9c9f2
Papawy <contactpapawy@gmail.com>
2016-10-14 02:23:39 +0200

    Added web server lists management (hosted and master)

---
+++ SAMP_Ex/SAMP_Ex/Class/WebLists.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SAMP_Ex
+{
+    class WebLists
+    {
+        public static bool Load(string internetListAddress, string hostedListAddress)
+        {
+            try
+            {
+                GenerateInternetList(internetListAddress);
+                GenerateHostedList(hostedListAddress);
+                // need to add verification
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public static void UpdateInternetList()
+        {
+            foreach (Server serv in InternetList)
+            {
+                serv.TotalUpdate();
+            }
+        }
+
+        public static void UpdateHostedList()
+        {
+            foreach (Server serv in HostedList)
+            {
+                serv.TotalUpdate();
+            }
+        }
+
+        public static bool GenerateInternetList(string website)
+        {
+            if(InternetList == null)
+            {
+                InternetList = new BindingList<Server>();
+                InternetList.RaiseListChangedEvents = true;
+            }
+            InternetList.Clear();
+
+            WebClient client = new WebClient();
+            string strList = client.DownloadString(new Uri(website));
+            try
+            {
+                foreach(string servip in strList.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    try
+                    {
+                        Server tmpServ = new Server(servip);
+                        if (tmpServ.IsValid) InternetList.Add(tmpServ);
+                    }
+                    catch (Exception ex) { continue; }        
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool GenerateHostedList(string website)
+        {
+            if (HostedList == null)
+            {
+                HostedList = new BindingList<Server>();
+                HostedList.RaiseListChangedEvents = true;
+            }
+            HostedList.Clear();
+
+            WebClient client = new WebClient();
+            string strList = client.DownloadString(new Uri(website));
+            try
+            {
+                foreach (string servip in strList.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    try
+                    {
+                        Server tmpServ = new Server(servip);
+                        if (tmpServ.IsValid) HostedList.Add(tmpServ);
+                    }
+                    catch (Exception ex) { continue; }
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #region Attributes       
+        public static BindingList<Server> InternetList;
+        public static BindingList<Server> HostedList;
+        #endregion
+    }
+}
